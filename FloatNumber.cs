598525e3497ad8455988901c8ea75c46de7a29e9
gharjare <gharjarechetan@gmail.com>
2022-10-28 17:50:16 +0530

    Added float max number

---
+++ FloatNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    public class FloatNumber
+    {
+        public static double MaximumFloatNumber(double firstvalue, double secondvalue, double thirdvalue )
+
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0)
+            {
+                return firstvalue;
+            } 
+            if(secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0)
+            {
+                return secondvalue;
+            }
+            if(thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0)
+            {
+                return thirdvalue;
+            }
+            throw new Exception("firstNumber , secondNumber , thirdNumber are same");
+        }
+
+    }
+}
