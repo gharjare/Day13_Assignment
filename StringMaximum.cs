498bed8d9d001252fcde3026e86f318dd9365345
gharjare <gharjarechetan@gmail.com>
2022-10-28 17:53:30 +0530

    added max string

---
+++ StringMaximum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    public class StringMaximum
+    {
+        public static string MaximumStringNumber(string firststring,string secondstring,string thirdstring)
+        {
+            if(firststring.CompareTo(secondstring) > 0 && firststring.CompareTo(thirdstring) > 0)
+            {
+                return firststring;
+            }
+            if(secondstring.CompareTo(firststring) > 0 && secondstring.CompareTo(thirdstring) > 0)
+            {
+                return secondstring;
+            }
+            if(thirdstring.CompareTo(thirdstring) > 0 && thirdstring.CompareTo(secondstring) > 0)
+            {
+                return thirdstring;
+            }
+            throw new Exception("firstNumber , secondNumber , thirdNumber are same");
+        }
+    }
+}
