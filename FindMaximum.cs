1c4872a83f673ef693960f453d2dd765902e9372
gharjare <gharjarechetan@gmail.com>
2022-10-28 17:49:16 +0530

    Added maximum number

---
+++ FindMaximum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    public class FindMaximum
+
+    {
+        public static int MaximumIntegerNumber(int firstvalue, int secondvalue, int thirdvalue)
+        {
+            if(firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0)
+            {
+                return firstvalue;
+            }
+            if(secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0)
+            {
+                return secondvalue;
+            }
+            if(thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0)
+            {
+                return thirdvalue;
+            }
+            throw new Exception("firstNumber , secondNumber , thirdNumber are same");
+            
+        }
+
+    }
+}
