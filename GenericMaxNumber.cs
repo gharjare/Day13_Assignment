ba881685d436f0caf1f5cc20ecb2a754052d26fe
gharjare <gharjarechetan@gmail.com>
2022-10-28 18:01:58 +0530

    added max generic number

---
+++ GenericMaxNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace TestMaximum
+{
+    public class GenericMaxNumber<T> where T : IComparable
+    {
+        public T[] value;
+
+        public GenericMaxNumber(T[] value)
+        {
+            this.value = value;
+        }
+        public T[] sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = sort(values);
+            return sorted_values[^1];
+        }
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("maximum value is " + max);
+        }
+    }
+}
